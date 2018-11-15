08cd8d952cb407321fb44d9bc31d32e99d68aacb
Scott <ssanford@mediafly.com>
2018-11-14 18:53:46 -0600

    add a side drawer and header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header_Footer/Header'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{height: '1500px', background: 'orangered'}}>
         <Header />
         hello
       </div>
